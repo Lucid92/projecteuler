a503b892d68b50b4b49ebb1ef9b18b4d7ca2c231
Beau <1762486+Lucid92@users.noreply.github.com>
2020-02-20 21:32:35 +0800

    problem 35

---
+++ python/35.py
@@ -0,0 +1,27 @@
+import lib.mathhelper as mathhelper
+
+def get_all_rotations(n):
+    rotations = [n]
+    for _ in range(len(str(n)) - 1):
+        rotations.append(rotate(rotations[-1]))
+
+    return rotations
+
+def rotate(n):
+    ''' Rotates a number to the left one position'''
+    n_str = str(n)
+    return int(n_str[1:] + n_str[0])
+
+primes = set(mathhelper.primes(1000000))
+
+results = []
+
+for prime in primes:
+    # Ignore primes with zeros since the rotation will cause an invalid number according to the question.
+    if '0' in str(prime):
+        continue
+
+    if all(i in primes for i in get_all_rotations(prime)):
+        results.append(prime)
+
+print(len(results))
