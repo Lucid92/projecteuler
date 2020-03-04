64f062f7385ef1435c9c4c4493c9de42f9fcd647
Beau <1762486+Lucid92@users.noreply.github.com>
2020-03-04 21:09:26 +0800

    problem 31

---
+++ python/31.py
@@ -0,0 +1,21 @@
+ways = 0
+
+def give_change(target, values):
+    global ways
+
+    for v in values:
+        new_target = target - v
+        if new_target < 0:
+            return
+        if new_target == 0:
+            ways += 1
+            return
+        else:
+            new_values = values[0:values.index(v) + 1]
+            give_change(new_target, new_values)
+
+v = [1, 2, 5, 10, 20, 50, 100, 200]
+give_change(200,v)
+
+print(ways)
+
